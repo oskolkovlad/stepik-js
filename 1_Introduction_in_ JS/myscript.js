0654831ffec0a423e84d1a644c1dd847bdc5e05d
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-16 21:10:56 +0300

    Тернарный оператор

--- 1_Introduction_in_ JS/myscript.js
+++ 1_Introduction_in_ JS/myscript.js
@@ -6,12 +6,15 @@ document.write("Hello World");
 	комментарий
 */
 
+
 // Объявление переменных
 var x1 = "dvsv"; 
 var studentName1;
+let studentName2;
+
+
 // Для того, чтобы определить тип данных, записанных в переменной, мы можем воспользоваться оператором typeof.
 alert(typeof x1); 
-let studentName2;
 
 
 // Функция
@@ -19,3 +22,17 @@ function SimpleMethod(a) {
 	var x = a;
 	return x;
 }
+
+// Функция суммы
+function Sum(a, b){
+	//return a + b;
+
+	let x = a + b;
+	return x;
+}
+
+
+// Тернарный оператор
+friend = "aaa";
+hiJack = (friend == "Jack") ? "Hi, Jack!" : "Hi";
+console.log(hiJack);
