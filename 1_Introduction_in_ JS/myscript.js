22c1abedf9ce4df3f2e77e3eb1ab5ef4369dde3d
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-16 20:42:26 +0300

    Переменные: локальные и глобальные, var и let

--- 1_Introduction_in_ JS/myscript.js
+++ 1_Introduction_in_ JS/myscript.js
@@ -1,1 +1,14 @@
+// Это текстовый комментарий - однострочный
 document.write("Hello World");
+
+/*
+	многострочный
+	комментарий
+*/
+
+// Объявление переменных
+var x1; 
+var studentName1;
+
+alert (typeof x1); 
+let studentName2;
