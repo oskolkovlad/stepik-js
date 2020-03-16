6b8242e939fefb804ae31e1d766e2367ddff2277
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-16 20:51:16 +0300

    Простая функция

--- 1_Introduction_in_ JS/myscript.js
+++ 1_Introduction_in_ JS/myscript.js
@@ -7,8 +7,15 @@ document.write("Hello World");
 */
 
 // Объявление переменных
-var x1; 
+var x1 = "dvsv"; 
 var studentName1;
-
-alert (typeof x1); 
+// Для того, чтобы определить тип данных, записанных в переменной, мы можем воспользоваться оператором typeof.
+alert(typeof x1); 
 let studentName2;
+
+
+// Функция
+function SimpleMethod(a) {
+	var x = a;
+	return x;
+}
